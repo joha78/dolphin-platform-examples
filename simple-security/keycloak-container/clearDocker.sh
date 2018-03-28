86aa912e7d8ae7ae42fc3c3f2edde0b8725c99b8
Hendrik Ebbers <hendrik.ebbers@web.de>
2018-03-28 13:03:30 +0200

    docker container renamed

--- simple-security/keycloak-container/clearDocker.sh
+++ simple-security/keycloak-container/clearDocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker rm -f $(docker ps -a -q -f "name=services_keycloak-server")
-docker rmi -f $(docker images -q -f "reference=services_keycloak-server" )
+docker rm -f $(docker ps -a -q -f "name=platform-security-sample-keycloak-server")
+docker rmi -f $(docker images -q -f "reference=platform-security-sample-keycloak-server" )
